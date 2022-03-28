e55f99e20cbe5dc07561cce60d8e0b1d588e161a
Prashant Gajjar <prashantg.spaceo@gmail.com>
2022-03-28 15:20:31 +0530

    tokn

--- MixpanelDemo/MixpanelDemo/AppDelegate.swift
+++ MixpanelDemo/MixpanelDemo/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         var ADD_YOUR_MIXPANEL_TOKEN_BELOW_🛠🛠🛠🛠🛠🛠: String
-        Mixpanel.initialize(token: "MIXPANEL_TOKEN")
+        Mixpanel.initialize(token: "b59e5a7ef68da4aae8b9276904650a7a")
         Mixpanel.mainInstance().loggingEnabled = true
 
         return true
